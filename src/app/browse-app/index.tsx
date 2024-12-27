f79edebdf5d8070dcbdd26d99fe41cd5ceff0d5d
farhan.ansari@pivotree.com <farhan.ansari@pivotree.com>
2024-12-27 17:48:33 +0530

    ix some issue

--- src/app/browse-app/index.tsx
+++ src/app/browse-app/index.tsx
@@ -1,23 +1,16 @@
 import React from 'react'
 import Header from '../../base-components/Header'
 // import Sidebar from '../../base-components/Sidebar'
-import Header from '../../base-components/Header'
-// import Sidebar from '../../base-components/Sidebar'
 import PageContainer from '../../page-container'
 import { AppSidebar } from '../../base-components/Sidebar'
+<<<<<<< HEAD
 import { SidebarProvider, SidebarTrigger } from '../../../components/ui/sidebar'
+=======
+import { SidebarProvider, SidebarTrigger } from '../../components/ui/sidebar'
+>>>>>>> ed491cacb1c95799c33e367e8dfc5ba6bf6fed49
 
 const BrowseApp: React.FC = () => (
   <>
-    <Header />
-    <div className="h-[90%] w-full flex font-[Poppins] overflow-hidden">
-      <SidebarProvider>
-        <AppSidebar />
-        <main className="h-full overflow-hidden">
-          <SidebarTrigger />
-          <PageContainer/>
-        </main>
-      </SidebarProvider>
     <Header />
     <div className="h-[90%] w-full flex font-[Poppins] overflow-hidden">
       <SidebarProvider>
